3888185ba733e3e77d551a7915e5812cf0484740
Fabio Marques <fabio.marquezzx@gmail.com>
2020-07-30 19:35:06 -0300

    Creating Account page

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
+  public isConfig: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
--- src/app/config/app-routing.module.ts
+++ src/app/config/app-routing.module.ts
@@ -4,12 +4,15 @@ import { LoginComponent } from '../components/login/login.component';
 import { RegisterComponent } from '@app/components/register/register.component';
 import { HomeComponent } from '@app/components/home/home.component';
 import { ProductSearchComponent } from '@app/components/product-search/product-search.component';
+import { AccountComponent } from '@app/components/account/account.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent},
   { path: 'registrar', component: RegisterComponent},
   { path: 'pesquisar', component: ProductSearchComponent},
+  { path: 'conta', component: AccountComponent},
+
 ];
 
 @NgModule({
